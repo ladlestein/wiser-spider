6ee6f371b636e59a6fcf93f449f8845e9039dbb9
Larry Edelstein <ladlestein@gmail.com>
2013-04-14 19:21:39 -0700

    more code

--- build.sbt
+++ build.sbt
@@ -9,10 +9,15 @@ scalaVersion := "2.10.1"
 fork in run := true
 
 libraryDependencies ++= Seq(
-  "com.nowanswers" %% "spider" % "0.1.3",
+  "com.nowanswers" %% "spider" % "0.1.4",
   "com.typesafe.akka" %% "akka-actor" % "2.1.2",
   "org.mongodb" %% "casbah" % "2.5.0",
+  "com.novus" %% "salat" % "1.9.2-SNAPSHOT",
   "com.github.tototoshi" %% "scala-csv" % "0.7.0",
   "io.spray" % "spray-client" % "1.1-M7",
   "commons-codec" % "commons-codec" % "1.7"
+)
+
+resolvers ++= Seq(
+    "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
 )
--- src/main/scala/WiserSpiderApp.scala
+++ src/main/scala/WiserSpiderApp.scala
@@ -1,22 +1,27 @@
 package com.nowanswers.wiserspider
 
-import akka.actor.{Props, Actor}
+import akka.actor.{ActorLogging, ActorSystem, Props, Actor}
 import akka.event.Logging
 import akka.pattern._
 import com.github.tototoshi.csv.CSVReader
 import java.io.File
 import java.net.URLEncoder
-import com.nowanswers.spider._
+import scala.concurrent.duration._
 import spray.http.{HttpRequest, HttpMethods}
 import spray.can.client.HttpClient
 import spray.client.HttpConduit
 import spray.io.IOExtension
 import akka.routing.RoundRobinRouter
-import scala.concurrent.{Await, Future}
-import akka.actor.Status.Success
+import scala.concurrent.{ExecutionContext, Await, Future}
 import java.security.MessageDigest
 import org.apache.commons.codec.binary.Hex
 import scala.concurrent.duration.Duration
+import org.ccil.cowan.tagsoup.jaxp.SAXFactoryImpl
+import akka.util.Timeout
+import com.novus.salat.annotations.raw.Key
+import com.mongodb.casbah.Imports._
+import akka.actor.Status.Failure
+import akka.actor.Status.Success
 
 /**
  * Created with IntelliJ IDEA.
@@ -25,14 +30,32 @@ import scala.concurrent.duration.Duration
  * Time: 5:37 PM
  * To change this template use File | Settings | File Templates.
  */
+
+
 object WiserSpiderApp extends App {
+
+  val system = ActorSystem("spider")
+
+  val dispatcher = system.dispatcher
+
+  implicit val ec = ExecutionContext.fromExecutor(dispatcher)
+
+  implicit val timeout = Timeout(1 hour)
+
+
+  case object Start
+
+  case class QueryIssue(issueName: String)
 
   val API_KEY = "5afb532c23893c3a932a38cce6d7b06f"
   val SECRET = "ec8efd4d64c84ab64c099642808393b8"
+  val digester = MessageDigest.getInstance("MD5")
 
   val issuesFilePath = "resources/issues.csv"
   val reader = CSVReader.open(new File(issuesFilePath))
   val rows = reader.all()
+
+  val parserFactory = new SAXFactoryImpl
 
   val issues = rows map { row =>
     val id = row(0).toInt
@@ -43,7 +66,7 @@ object WiserSpiderApp extends App {
     Issue(id, topLevelName, name, rating)
   }
 
-  val nApiCallers = 3
+  val nApiCallers = 1
   val hostname = "www.wiser.org"
   val ioBridge = IOExtension(system).ioBridge()
   val httpClient = system.actorOf(Props(new HttpClient(ioBridge)))
@@ -56,10 +79,9 @@ object WiserSpiderApp extends App {
 
   val master = system.actorOf(Props[Master], name = "Master")
 
-  (master ? Start) onComplete { _ =>
-    println("all results in, calling System.exit")
-    System.exit(0)
-  }
+  Await.result(master ? Start, Duration.Inf)
+  println("all results in, calling System.exit")
+  System.exit(0)
 
   class Master extends Actor {
 
@@ -67,45 +89,99 @@ object WiserSpiderApp extends App {
 
     val apiCallers = context.actorOf(Props[PageVisitor].withRouter(RoundRobinRouter(nApiCallers)), name = "letterRouter")
 
-    val template = s"/organizations/api_search?phrase=%s&sig=%s&key=$API_KEY"
-
-    val digester = MessageDigest.getInstance("MD5")
     def receive = {
       case Start => {
         val cs = sender
-        val tasks = Future.sequence(
-          for (issue <- issues take 2)
-          yield {
-            log info s"fetching issue ${issue.name}"
-            val query = URLEncoder.encode(issue.name, "UTF-8")
-            val digest = Hex.encodeHexString(digester.digest(("phrase" + issue.name + SECRET).getBytes))
-            val url = template.format(query, digest)
-            apiCallers ? VisitPage(url)
+
+        try {
+          val tasks = Future.sequence(
+            for (issue <- issues take 2)
+            yield {
+              log info s"fetching issue ${issue.name}"
+              apiCallers ? QueryIssue(issue.name)
+            }
+          )
+          Await.result(tasks, Duration.Inf)
+          log info "all results in"
+          cs ! Success
+        }
+        catch {
+          case e: Exception => {
+            cs ! Failure(e)
+            throw e
           }
-        )
-        Await.result(tasks, Duration.Inf)
-        log info "all results in"
+        }
       }
     }
   }
 
-  class PageVisitor extends Actor {
+  class PageVisitor extends Actor with RealOrganizationStoreComponent with ActorLogging {
 
-    val log = Logging(context.system, this)
+    val collection = {
+      val connection = MongoConnection()
+      val database = connection("spider")
+      database ("wiserorgs")
+    }
+
+    val UrlTemplate = s"/organizations/api_search?phrase=%s&sig=%s&key=$API_KEY"
 
     def receive = {
-      case VisitPage(url) => {
+      case QueryIssue(issueName) => {
+        val query = URLEncoder.encode(issueName, "UTF-8")
+        val digest = Hex.encodeHexString(digester.digest(("phrase" + issueName + SECRET).getBytes))
+        val url = UrlTemplate.format(query, digest)
+
         val cs = sender
-        val resP = pipeline(HttpRequest(method = HttpMethods.GET, uri = url))
+        try {
+          val resP = pipeline(HttpRequest(method = HttpMethods.GET, uri = url))
 
-        resP map {
-          res => {
-            res.status.isSuccess
-            val body = res.entity.asString
-            log info s"have body for $url"
+          val parser = parserFactory.newSAXParser
 
-            log info body
-            cs ! Success
+
+          resP map {
+            res => {
+              res.status.isSuccess match {
+
+                case true => {
+                  log info s"have body for $url"
+
+                  val loader = xml.XML.withSAXParser(parser)
+                  val doc = loader.loadString(res.entity.asString)
+                  val seq = doc \\ "organization"
+
+                  seq foreach {
+                    orgNode =>
+                      val name = (orgNode \ "@name") text
+                      val id = (orgNode \ "@id") text
+                      val areas = ((orgNode \ "@area") text) split ","
+
+                      if (areas contains issueName) {
+                        val org = Organization(id, name, areas toSet)
+
+                        datastore save org
+                        log info s"saving $org"
+                      } else {
+                        log info s"ignoring org with name $name because it doesn't have issue $issueName"
+                      }
+                  }
+
+                  cs ! Success
+                }
+
+                case _ => {
+                  log error s"Got status ${res.status} fetching $url"
+                  log error s"message: ${res.message}"
+                  cs ! Failure
+                }
+
+              }
+            }
+          }
+        }
+        catch {
+          case e: Exception => {
+            cs ! Failure(e)
+            throw e
           }
         }
       }
@@ -115,9 +191,40 @@ object WiserSpiderApp extends App {
 }
 
 
+case class OrgDTO(@Key("_id") id: String, name: String, areas: Set[String])
+object OrgDTO {
+  def apply(org: Organization): OrgDTO = OrgDTO(org.id, org.name, org.issues )
+}
+
+trait OrganizationStoreComponent {
+
+  val datastore: OrganizationStore
+
+  trait OrganizationStore {
+     def save(org: Organization)
+  }
+}
+
+trait RealOrganizationStoreComponent extends OrganizationStoreComponent {
+
+  self: ActorLogging =>
+
+  import com.novus.salat._
+  import com.novus.salat.global._
+  import com.mongodb.casbah.MongoCollection
+
+  def collection: MongoCollection
+
+  lazy val datastore: OrganizationStore = new OrganizationStore{
+    def save(org: Organization) {
+      val dto = OrgDTO(org)
+      val mob = grater[OrgDTO].asDBObject(dto)
+      log info s"inserting ${org.name}"
+      collection insert mob
+      log info s"inserted ${org.name}"
+    }
+  }
+}
 
 case class Issue(id: Integer, topLevelName: String, name: String, rating: Integer)
-
-// Some code to go through all the issues in Brian's spreadsheet
-
-// Some code to fetch
+case class Organization(id: String, name: String, issues: Set[String])
